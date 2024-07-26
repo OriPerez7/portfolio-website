a02e0546fd6a47a8c7a69b0b10d2e1ce666230ad
Orian Perez <dylanperezart@gmail.com>
2024-07-26 18:18:53 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <h1>Title</h1>
+      <h2>Subtitle</h2>
+      <h3>Header 2</h3>
+      <p>Body</p>
+      <button className="btn-primary">Primary</button>
+      <button className="btn-secondary">secondary</button>
+      <input placeholder="Please enter input"/>
+    </>
+  );
+}
+
+export default App;
