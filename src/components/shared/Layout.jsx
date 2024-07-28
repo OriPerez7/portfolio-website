7520786bcecc76583fcb76d7e87e5282f4cd25f5
Orian Perez <dylanperezart@gmail.com>
2024-07-28 12:00:56 +0200

    Create website layout and scroll to top arrow

---
+++ src/components/shared/Layout.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import ScrollToTop from "./ScrollToTop";
+
+export const Divider = () => {
+  return (
+    <div className="md:xl-mx-20 my-20">
+      <div className="w-full h-[0.5px] bg-gray"></div>
+    </div>
+  );
+};
+
+export const SectionLayout = ({ title, subtitle, children }) => {
+  return (
+    <div>
+      <Divider />
+
+      <div className="flex flex-col justify-center items-start md:items-center">
+        <div className="md:text-center md:mb-10 md:px-40 mb-5">
+          <h2>{title}</h2>
+          <p>{subtitle}</p>
+        </div>
+
+        {children}
+      </div>
+    </div>
+  );
+};
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex flex-col h-screen">
+      <Navbar />
+      {children}
+      <Divider />
+      <ScrollToTop />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
