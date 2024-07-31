65aee645e0ba9971db720a4741cb7e30897f899b
Orian Perez <dylanperezart@gmail.com>
2024-07-31 07:28:50 +0200

    Add projects section link

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,10 +1,12 @@
 import Hero from "../components/home/Hero"
+import ProjectsSection from "../components/home/Projects/Projects"
 import Layout from "../components/shared/Layout"
 
 const Home = () => {
   return (
     <Layout>
         <Hero />
+        <ProjectsSection />
     </Layout>
   )
 }
