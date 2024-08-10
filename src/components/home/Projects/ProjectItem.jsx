912629f669adbf322facd4a13d6c55b20f16ea63
Orian Perez <dylanperezart@gmail.com>
2024-08-10 10:39:59 +0200

    Add projects images and round buttons

--- src/components/home/Projects/ProjectItem.jsx
+++ src/components/home/Projects/ProjectItem.jsx
@@ -16,14 +16,14 @@ const ProjectItem = ({
   return (
     <div>
       <div className="min-w-[300px] max-w-[400px] cursor-pointer shadow-md transition ease-in-out delay-100 hover:scale-110 duration-200 flex flex-col gap-3">
-        <img src={image} alt={`${title} image`} />
+        <img src={image} alt={`${title} image`} className="rounded-xl" />
         <div className="flex gap-4">
           {tag} {otherTag}
         </div>
         <h3 className="mb-3">{title}</h3>
         <p className="text-md mb-4">{description}</p>
         <div className="flex gap-10 justify-end">
-          <button className="btn-primary" onClick={openNewPreviewLink}>
+          <button className="btn-primary rounded-full" onClick={openNewPreviewLink}>
             Live preview
             <RxOpenInNewWindow className="inline ml-1.5 text-lg text-white" />
           </button>
--- src/components/home/Projects/projectsList.js
+++ src/components/home/Projects/projectsList.js
@@ -2,13 +2,15 @@ import { ReactTag, NextJSTag, TailwindTag } from './Tag';
 import { TAGS } from '../../constants';
 
 import logo from '../../../assets/logo.png';
+import binaryBeans from '../../../assets/binary-beans-cover.png';
+import natureQuest from '../../../assets/camping-app-cover.png';
 
 export const projects = [
     {
       title: 'Binary Beans',
       description:
         'Coffee Shop for developers. Created with React, Tailwind and Framer Motion.',
-      image: logo,
+      image: binaryBeans,
       tagIcon: ReactTag,
       otherTag: TailwindTag,
       tag: TAGS.frontend,
@@ -18,7 +20,7 @@ export const projects = [
       title: 'Nature Quest',
       description:
         'Camping App landing page. Created with Next.js (TypeScript) and Tailwind.',
-      image: logo,
+      image: natureQuest,
       tagIcon: NextJSTag,
       otherTag: TailwindTag,
       tag: TAGS.frontend,
